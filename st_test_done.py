f30146891c874ce1720498e587a204c17e6df93c
Thitiphat <arm.orm@hotmail.com>
2021-09-24 12:57:00 +0700

    updated select type

--- st_test_done.py
+++ st_test_done.py
@@ -9,6 +9,8 @@ import keras
 import os
 import time
 from stqdm import stqdm
+from zipfile import ZipFile
+from PIL import Image
 
 #defined setting
 WIDTH = 128
@@ -19,48 +21,6 @@ def load_model(model_name):
     model = keras.models.load_model(os.path.join(model_path , model_name))
     return model
 
-# set function
-def img2data(path,label):
-    rawImgs = []
-    labels = []
-    name = []
-    c = 0
-    for imagePath in (path):
-        for item in stqdm(os.listdir(imagePath)):
-            file = os.path.join(imagePath, item)
-            name.append(item)
-            c+=1
-            l = item.split('_')[0]
-            if l == label[0]: labels.append([1,0,0])
-            elif l == label[1]: labels.append([0,1,0])
-            elif l == label[2]: labels.append([0,0,1])
-            img = cv2.imread(file , cv2.COLOR_BGR2RGB)
-            img = cv2.resize(img ,(WIDTH,WIDTH))
-            rawImgs.append(img)
-            time.sleep(0.03)
-    return rawImgs, labels, name
-
-def get_results(model,x,label):
-    results = model.predict(x)
-    predict = []
-    for i in range(len(results)) :
-        predict.append(label[np.argmax(results[i])])
-    return predict
-
-def show_data(x_test, pics_name, prediction):
-    name_pic = st.selectbox('select pics_name',pics_name)
-    name_pic = pics_name[0]
-    position_pic = pics_name.index(name_pic)
-    st.image(cv2.cvtColor(x_test[position_pic], cv2.COLOR_BGR2RGB),width=400)
-    st.write('Prediction of selected picture is : ',prediction[position_pic])
-
-
-def check(x_test, name_test, predict_aug):
-    # select picture to check
-    check = st.button('Please click this massage if you want to see specific picture and result')
-    if check:
-        show_data(x_test, name_test, predict_aug)
-
 def get_dirname():
     # Set up tkinter
     root = tk.Tk()
@@ -68,10 +28,87 @@ def get_dirname():
     
     # Make folder picker dialog appear on top of other windows
     root.wm_attributes('-topmost', 1)
-    dirname = st.sidebar.text_input('Selected folder:', filedialog.askdirectory(master=root))
+    dirname = st.text_input('Selected folder:', filedialog.askdirectory(master=root))
     return dirname
 
+# set function
+def img2data_directory(path):
+    rawImgs = []
+    name = []
+    for imagePath in (path):
+        for item in stqdm(os.listdir(imagePath)):
+            file = os.path.join(imagePath, item)
+            name.append(item)
+            img = cv2.imread(file , cv2.COLOR_BGR2RGB)
+            img = cv2.resize(img ,(WIDTH,WIDTH))
+            rawImgs.append(img)
+            time.sleep(0.03)
+    return rawImgs, name
 
+def img2data_zip(zip_file):
+    rawImgs = []
+    name = []
+    archive = ZipFile(zip_file, 'r')
+    files = archive.infolist()
+    name_list = archive.namelist()
+    for i in range(len(name_list)):
+        img_data = archive.open(files[i])
+        #img = cv2.imread(img_data , cv2.COLOR_BGR2RGB)
+        PIL_img = Image.open(img_data)
+        img = np.array(PIL_img) # change PIL to numpy
+        img = img[:, :, ::-1].copy() 
+        img = cv2.resize(img ,(WIDTH,WIDTH))
+        rawImgs.append(img)
+        name.append(name_list[i])
+        time.sleep(0.03)
+    return rawImgs, name
+
+def img2data_image(file):
+    rawImgs = []
+    name = []
+    PIL_img = Image.open(file)
+    img = np.array(PIL_img) # change PIL to numpy
+    img = img[:, :, ::-1].copy() 
+    img = cv2.resize(img ,(WIDTH,WIDTH))
+    rawImgs.append(img)
+    name = str(file)
+    return rawImgs, name
+    
+def choose_type_input():
+    st.title('Select input type')
+    input_type = st.selectbox('Select',['image','zip','directory'])
+    if input_type:
+        if input_type=='image':
+            input = st.file_uploader('Pls upload image file',type='png')
+        elif input_type=='zip':
+            input = st.file_uploader('Pls upload zip file',type='zip')
+        else : 
+            input = get_dirname()
+    return input_type, input, True
+
+def get_results(model,x,label):
+    results = model.predict(x)
+    predict = []
+    for i in range(len(results)):
+        predict.append(label[np.argmax(results[i])])
+    return predict
+
+def show_data(x_test, pics_name, prediction):
+    name_pic = st.selectbox('select pics_name', pics_name)
+    #name_pic = pics_name[0]
+    if name_pic:
+        position_pic = pics_name.index(name_pic)
+        st.image(cv2.cvtColor(x_test[position_pic], cv2.COLOR_BGR2RGB),width=400)
+        st.write('Prediction of selected picture is : ',prediction[position_pic])
+
+
+def check(x_test, pics_name, prediction):
+    # select picture to check
+    check = st.button('Please click this massage if you want to see specific picture and result')
+    if check:
+        show_data(x_test, pics_name, prediction)
+
+#@st.cache(suppress_st_warning=True)
 def strict_part():
     # Details of Application
     st.title('Glaucoma diagnosis Application')
@@ -90,40 +127,42 @@ def strict_part():
         'https://www.reviewofoptometry.com/CMSImagesContent/2011/11/030_RO1111_F1.gif',
         width=200,
     )
-    
-    # Folder picker button
-    st.sidebar.title('Folder Picker')
-    st.sidebar.write('Please select a folder of image:')
-    clicked = st.sidebar.button('Folder Picker')
-    return clicked
 
 #@st.cache(suppress_st_warning=True)
-def main_computational(dirname, model):
+def main_computational(input_type, input, model):
     start_time = time.time()
-        
-    x_test, y_test, name_test = img2data([dirname],label)
+    
+    if input_type=='image':
+        x_test, name_test = img2data_image(input)
+    elif input_type=='zip':
+        x_test, name_test = img2data_zip(input)
+    else:
+        x_test, name_test = img2data_directory([input])
+    
     x_test = np.array(x_test)
     x_test = x_test.astype('float32')
     x_test /= 255
     
-    predict_aug = get_results(model,x_test,label)
-    dict = {'pics_name': name_test, 'prediction': predict_aug} 
+    predicted = get_results(model,x_test,label)
+    dict = {'pics_name': name_test, 'prediction': predicted} 
     df = pd.DataFrame(dict)
     st.title('Results')
     st.dataframe(df)
     st.write("Total run time : --- %s seconds ---" % (time.time() - start_time))
-    return x_test, name_test, predict_aug
+    return x_test, name_test, predicted, True
 
 def main():
+    strict_part()
+    
     # Load model
     model = load_model('MbN_AEN_N_aug')
-    show_strict = strict_part()
-    
-    if show_strict:
-        dirname = get_dirname() #
-        x_test, name_test, predict_aug = main_computational(dirname, model)
-        #check(name_test, predict_aug)
-        show_data(x_test, name_test, predict_aug)
+    # select input type
+    input_type, input, done_select = choose_type_input()
+    if done_select :
+        x_test, name_test, predict_aug, done_process = main_computational(input_type, input, model)
+        if done_process:
+            #check(x_test, name_test, predict_aug)
+            show_data(x_test, name_test, predict_aug)
 
 if __name__ == '__main__':
 	main()
